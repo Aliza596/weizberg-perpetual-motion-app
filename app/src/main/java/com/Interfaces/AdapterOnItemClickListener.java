d8601ebc63bbe76b80460147ea4c915635377f59
Alison Weizberg <aweizber@student.touro.edu>
2024-04-01 19:21:50 -0400

    initial commit

---
+++ app/src/main/java/com/Interfaces/AdapterOnItemClickListener.java
@@ -0,0 +1,12 @@
+package com.Interfaces;
+
+import android.view.View;
+
+/**
+ * Used to send data out of the Adapter to the host Activity
+ */
+
+public interface AdapterOnItemClickListener
+{
+    void onItemClick(int position, View viewClicked);
+}
